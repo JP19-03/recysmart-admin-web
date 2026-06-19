65eca01f48eceabe8decfd626389fda82f64ce58
JP19-03 <u202014511@upc.edu.pe>
2026-06-19 13:08:24 -0500

    feat: add Label component and update package dependencies

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export * from './badge';
+export * from './button';
+export * from './combobox';
+export * from './input-group';
+export * from './input';
+export * from './label';
+export * from './progress';
+export * from './textarea';
+
