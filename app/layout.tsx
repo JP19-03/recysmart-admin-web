e06232cee84d11365407c15c3254978fd84f0165
JP19-03 <u202014511@upc.edu.pe>
2026-06-19 11:51:48 -0500

    feat: add metadata to layout and implement QueryProvider for React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "RecySmart | Admin Dashboard",
+  description: "Panel de administración central para la plataforma RecySmart: Gestión de basureros IoT, gamificación y usuarios en Lima Metropolitana.",
+  applicationName: "RecySmart Admin",
+  authors: [{ name: "RecySmart Team" }],
+};
 
 export default function RootLayout({
   children,
